2d149a53ac53dc1bd9203936667516f7f080b7e8
ChristopherBennett <chrisjwbennett@gmail.com>
2024-12-12 17:53:56 +0000

    Add files

---
+++ Code/C++/cQueue.cpp
@@ -0,0 +1,28 @@
+/*
+ * Christopher Bennett
+ * Circular Queue in C++ 
+ *
+*/
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+class Queue
+{
+	double arr[10];
+};
+
+
+
+int main()
+{
+	cout << "We Ball" << endl;
+
+	return 0;
+}
+
+
